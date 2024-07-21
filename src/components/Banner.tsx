65a158e88e91aea66e25776b13b86ddbce371da4
Semora <senalweerasekara@gmail.com>
2024-07-21 15:47:45 +0530

    saving changes

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,16 +5,16 @@ function rightSide() {
   return (
     <div className="text-white">
       <div >
-        <div className="text-xl md:text-2xl lg:text-4xl leading-snug md:leading-normal lg:leading-snug">Hola</div>
-        <div className="text-2xl md:text-4xl lg:text-6xl leading-snug md:leading-normal lg:leading-snug">
+        <div className="text-3xl md:text-3xl lg:text-5xl leading-snug md:leading-normal lg:leading-snug">Hola</div>
+        <div className="text-5xl md:text-5xl lg:text-7xl leading-snug md:leading-normal lg:leading-snug">
           I'm <span className="text-red-700">Senal</span>
         </div>
-        <div className="text-xl md:text-2xl lg:text-4xl font-bold leading-snug md:leading-normal lg:leading-snug">
+        <div className="text-3xl md:text-4xl lg:text-4xl font-bold leading-snug md:leading-normal lg:leading-snug">
           Full Stack Developer
         </div>
       </div>
       <div>
-        <div className="text-sm md:text-base lg:text-lg">T T F I</div>
+        <div className="text-xl md:text-xl lg:text-3xl leading-snug md:leading-normal lg:leading-snug">T T F I</div>
       </div>
     </div>
   );
@@ -24,13 +24,13 @@ const Banner: React.FC = () => {
   return (
     <div className="bg-black py-16">
       <div className="bg-black p-4 mx-auto container">
-        <div className="flex flex-col md:flex-row lg:flex-row gap-6 md:gap-12 lg:gap-20">
+        <div className="flex flex-col sm:flex-row md:flex-row lg:flex-row gap-6 md:gap-24 lg:gap-36">
           <div
             className={`w-full flex justify-center md:justify-end lg:justify-end`}
           >
             <img
               src={dpImg}
-              className="w-[200px] md:w-[300px]  lg:w-[400px]"
+              className="w-[300px] md:w-[300px]  lg:w-[400px]"
               alt="dpImg"
             />
           </div>
