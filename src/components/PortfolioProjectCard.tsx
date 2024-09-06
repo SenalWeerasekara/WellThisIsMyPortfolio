e953bb7a334249d43825e1ce55357e594e829bc0
Semora <senalweerasekara@gmail.com>
2024-09-06 22:57:59 +0530

    changed routing method

--- src/components/PortfolioProjectCard.tsx
+++ src/components/PortfolioProjectCard.tsx
@@ -8,6 +8,8 @@ interface PortfolioProject {
     repoLink?: string[];
     tech?: string[];
   }
+
+const commonBtn = 'text-lg inline-block px-4 py-2 rounded transition duration-300'
   
 function PortfolioCard({
     startDate,
@@ -17,13 +19,16 @@ function PortfolioCard({
     img,
     projectLink,
     repoLink,
-    tech
+    tech,
   }: PortfolioProject) {
     return (
-      <div className="bg-stone-800 p-2 rounded-2xl m-2">
+      <div className="bg-stone-800 p-4 rounded-2xl ">
         <div><img src={img} className='rounded-lg' alt="headerImg" /></div>
         <div className="mb-4">{name}</div>
         <div className="mb-4">{smallDes}</div>
+        <div>
+          <div className={`border text-purple-500 hover:text-white border-purple-500 hover:bg-purple-600 text-white px-4 py-2  ${commonBtn}`}>GitHub</div>
+        </div>
       </div>
     );
   }
